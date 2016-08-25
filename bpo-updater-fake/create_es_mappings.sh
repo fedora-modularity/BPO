58accd8990c6a224a99f2de1ac4a5f5902098dfb
Adam Samalik <asamalik@redhat.com>
2016-08-25 11:37:37 +0200

    update elasticsearch mappings

--- bpo-updater-fake/create_es_mappings.sh
+++ bpo-updater-fake/create_es_mappings.sh
@@ -44,9 +44,60 @@ curl -XPOST elasticsearch:9200/modularity -d '{
                     "type": "string",
                     "index": "not_analyzed"
                 },
-                "dependencies-build": {
+                "dependencies_build": {
                     "type": "string",
                     "index": "not_analyzed"
+                },
+                "components": {
+                    "type": "object",
+                    "properties": {
+                        "rpms": {
+                            "type": "object",
+                            "properties": {
+                                "name": {
+                                    "type": "string",
+                                    "index": "not_analyzed"
+                                },
+                                "version": {
+                                    "type": "string",
+                                    "index": "not_analyzed"
+                                },
+                                "release": {
+                                    "type": "string",
+                                    "index": "not_analyzed"
+                                },
+                                "build_state": {
+                                    "type": "string",
+                                    "index": "not_analyzed"
+                                }
+                            }
+                        }
+                    }
+                },
+                "api": {
+                    "type": "object",
+                    "properties": {
+                        "rpms": {
+                            "type": "string",
+                            "index": "not_analyzed"
+                        }
+                    }
+                },
+                "install_profiles": {
+                    "type": "object",
+                    "properties": {
+                        "name": {
+                            "type": "string",
+                            "index": "not_analyzed"
+                        },
+                        "description": {
+                            "type": "string"
+                        },
+                        "rpms": {
+                            "type": "string",
+                            "index": "not_analyzed"
+                        }
+                    }
                 }
             }
         }
